12b861f887d91e9f0b6c9b375ee9a49882e93b30
Vasco Neves <57102041+vasconevesxd@users.noreply.github.com>
2023-03-03 22:22:47 +0000

    fix some setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue';
+import { RouterView } from 'vue-router'
+import Navbar from './components/base/Navbar.vue'
+import Footer from './components/base/Footer.vue'
 </script>
 
 <template>
-
+  <Navbar />
   <RouterView />
+  <Footer />
 </template>
-
